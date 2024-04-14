b355b38d10ff88c67ae43e6439f44cc3ae200871
fatenaldousari <fatennald@gmail.com>
2024-04-14 10:16:57 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string[] phone = ["111"];
+string name = "faten";
+double gpa = 2.2;
+int age = 20;
+bool YesOrNo = false;
+Console.WriteLine($"Hello,{name} age: {age} Gpa: {gpa} ?: {YesOrNo}");
+int number = 5;
+double result = Convert.ToDouble(number);
+Console.WriteLine($"result = {result}");
+var studentName = "faten";
+var favSubject = "math";
+var width = 5;
+var hight = 5;
+var area = width * hight;
+Console.WriteLine($"name {studentName} subject {favSubject} area {area}");
+
+
+
